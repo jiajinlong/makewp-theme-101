6103c942a6c32f104a188bca9905c980813a47a4
jiajinlong <147525878@qq.com>
2015-05-17 00:10:15 +0800

    Wordpress页面别名和自定义页面样式是有关系的。

---
+++ page-about-me.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+    <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <p><?php the_content(); ?></p>
+            <h3><?php the_title(); ?></h3>
+            <hr>
+        <?php endwhile; ?>
+    <?php endif; ?>
+<?php get_footer(); ?>
